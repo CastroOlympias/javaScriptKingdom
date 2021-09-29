0fa4ef14f0d918c0db58a6f2f3bf9c9ec8751579
CastroOlympias <blackcarrera@msn.com>
2021-09-28 21:09:12 -0600

    removed addEventListerner with method window.location.replace aappended button to new achor element that links to index 2 and visa versa, should now work on github pages instead of 404 for the second index

--- assets/index2/javaScript/main/main.js
+++ assets/index2/javaScript/main/main.js
@@ -4,9 +4,6 @@ mainHeadCssLink.rel = "stylesheet";
 mainHeadCssLink.href = "./assets/index2/cssStyles/main/main.css";
 bodyHead.append(mainHeadCssLink)
 
-
-
-
 const main = document.createElement('main');
 main.id = 'main';
 bodyEl.append(main)
@@ -15,18 +12,13 @@ const mainH1El = document.createElement('h1');
 mainH1El.textContent = 'INDEX 2 MAIN';
 main.append(mainH1El);
 
+const anch = document.createElement('a')
+anch.id = 'goTo'
+anch.setAttribute ('href', './index.html');
+main.append(anch)
+
+const anchButton = document.getElementById('goTo')
 const button = document.createElement('button');
 button.id = 'button';
 button.textContent = 'CLICK TO GO TO INDEX 1';
-main.append(button)
-
-
-
-
-
-document.getElementById("button").addEventListener("click", function(event){
-    event.preventDefault()
-    window.location.href = "../../../../index.html"
-    
-})
-
+anch.append(button)
