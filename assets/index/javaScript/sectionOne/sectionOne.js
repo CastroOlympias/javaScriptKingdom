ef60b5944a1f7af30c8e831b76ae28d54347aa12
CastroOlympias <blackcarrera@msn.com>
2021-09-29 19:13:53 -0600

    added ability to click a button to create an element that says hey or something on index.html

--- assets/index/javaScript/sectionOne/sectionOne.js
+++ assets/index/javaScript/sectionOne/sectionOne.js
@@ -1,15 +1,29 @@
-const sectionOne = function() {
+
+const sectionOne = function () {
 
     const bodyHead = document.getElementById('head');
     const bodyEl = document.getElementById('body');
-    
+
     const sectionOne = document.createElement('section');
     sectionOne.id = 'sectionOne';
     bodyEl.append(sectionOne)
-    
+
     const sectionOneH1El = document.createElement('h1');
     sectionOneH1El.textContent = 'INDEX 1 SECTION ONE'
     sectionOne.append(sectionOneH1El)
-    
+
+    const loadSectionTwo = document.createElement('button');
+    loadSectionTwo.textContent = 'LOAD SECTION TWO';
+    sectionOne.append(loadSectionTwo)
+
+    const sectionTwo = function() {
+        const number = '6'
+        console.log(number)
+        const testH1El = document.createElement('h1')
+        testH1El.textContent = 'Hey there!';
+        sectionOne.append(testH1El)
+    }
+    loadSectionTwo.onclick = sectionTwo;
+
 }
 sectionOne()
