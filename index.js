c6e1c22c9d28a929a74a830b05d3d42c8c868f9a
CastroOlympias <blackcarrera@msn.com>
2021-10-01 20:56:00 -0600

    created an animation fade-in for the App List menu element

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// These getElements are global and accessable to any script that gets loaded
+// These getElements are globally to any script that gets loaded
 const rootHtml = document.getElementById('rootHtml');
 const rootHead = document.getElementById('rootHead');
 const rootBody = document.getElementById('rootBody');
-
 // Loads the index.css file to style the root of the application
 const indexCss = document.createElement('link')
 indexCss.rel = 'stylesheet';
 indexCss.href = './index.css';
 rootHead.append(indexCss)
+// script elements here for auto loading section type parent elements not trigger loaded by running functions
 
-// Loads the root main element section
-const rootMain = document.createElement('main');
-rootMain.id = 'rootMain';
-rootBody.append(rootMain)
+// Main element auto loaded here by sript
+const rootMainScript = document.createElement('script');
+rootMainScript.setAttribute('src', './modules/main/main.js');
+rootHtml.append(rootMainScript);
 
-// Nabigation bar is globalally loaded via a havaScript
+// Nabigation bar is globalally loaded via a javaScript
 const navBarModule = document.createElement('script');
 navBarModule.setAttribute('src', './modules/navBar/navBar.js')
 rootHtml.append(navBarModule)
--- modules/navBar/appListModal.js
+++ modules/navBar/appListModal.js
@@ -6,3 +6,14 @@ const text = document.createElement('h1')
 text.id = 'text'
 text.textContent = "Hey dude, you killed a mouse, you shouldn't have!";
 appListModal.append(text)
+
+// const anch = document.createElement('a')
+// anch.id = 'goTo'
+// anch.setAttribute('href', './index2.html');
+// appListModal.append(anch)
+
+// const anchButton = document.getElementById('goTo')
+// const button = document.createElement('button');
+// button.id = 'button';
+// button.textContent = 'CLICK TO GO TO INDEX 2';
+// anch.append(button)
